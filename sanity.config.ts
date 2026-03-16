faf0e0029f608d83880346682af155a6b3beca75
sebaasr0 <s.rodriguez28@ncf.edu>
2026-03-16 18:44:09 -0400

    feat: add structureTool and visionTool to Sanity Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
 import { projectSchema } from './sanity/schemas/project'
 import { studioSchema } from './sanity/schemas/studio'
 
@@ -8,6 +10,22 @@ export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   basePath: '/studio',
+  plugins: [
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title('Contenido')
+          .items([
+            S.listItem()
+              .title('Proyectos')
+              .child(S.documentTypeList('project').title('Proyectos')),
+            S.listItem()
+              .title('Estudio / Sobre mí')
+              .child(S.documentTypeList('studio').title('Estudio')),
+          ]),
+    }),
+    visionTool(),
+  ],
   schema: {
     types: [projectSchema, studioSchema],
   },
